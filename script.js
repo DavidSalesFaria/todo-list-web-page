884b0411d903138575a883b770e97b028a888b1f
DavidSheltonSF <david.faria012@gmail.com>
2024-03-18 20:29:25 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+document.querySelector('.js-todo-add-button')
+  .addEventListener('click', () => {
+    addTodo();
+  })
+
+// Almost finishing: 9:26:38
+function renderTodoList(){
+    const todoListElement = document.querySelector('.js-todo-list')
+
+    let todoListHTML = ``;
+    
+    todoList.forEach((todoObject, index) => {
+        
+        const {name, dueDate} = todoObject;
+
+        todoListHTML += `
+
+        <span class='name'>${name}</span>
+        <span class='dueDate'>${dueDate}</span>
+        <span>
+             
+            <button class="delete-button js-delete-todo-button">Delete</button>
+        </span>`
+        ;
+    });
+
+    todoListElement.innerHTML = todoListHTML;
+
+    document.querySelectorAll('.js-delete-todo-button')
+      .forEach((deleteButton, index) => {
+        deleteButton.addEventListener('click', () => {
+          todoList.splice(index, 1);
+          renderTodoList();
+        })
+      })
+}
+
+
+function addTodo(){
+    const inputNameElement = document.querySelector('.js-name-input');
+    const inputDateElement = document.querySelector('.js-due-date-input'); 
+
+    const name = inputNameElement.value;
+    const dueDate = inputDateElement.value;
+
+    // Using shorthand property
+    todoList.push({
+        name,
+        dueDate
+    });
+
+    inputNameElement.value = "";
+    inputDateElement.value = "";
+
+    renderTodoList();
+}
+let todoList = [];
+renderTodoList();
+
